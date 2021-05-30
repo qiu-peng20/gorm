f669f2b755d0eeed455a6eb3040bd94b00f89ad8
peng <973942322@qq.com>
2021-05-30 17:42:58 +0800

    new

--- cmd_test/main.go
+++ cmd_test/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "gorm"
+import (
+	"fmt"
+	"gorm"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main()  {
 	engine ,_ := gorm.NewEngine("mysql", "root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8")
 	defer engine.Close()
 
 	s := engine.NewSession()
-	s.Raw("")
+	_, _ = s.Raw("DROP TABLE IF EXISTS User").Exec()
+	_, _ = s.Raw("CREATE TABLE User(name CHAR(10))").Exec()
+	_, _ = s.Raw("CREATE TABLE User(name CHAR(10))").Exec()
+	result, _ := s.Raw("INSERT INTO User(name) value (?), (?)", "tom", "Sam").Exec()
+	count, _ := result.RowsAffected()
+	fmt.Print(count)
 }
